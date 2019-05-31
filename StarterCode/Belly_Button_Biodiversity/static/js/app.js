8e4b6b942b13063311602e88fda47129294f6990
Omar-Castillo <omar.castillo09@gmail.com>
2019-05-31 16:06:20 -0500

    addition of pie and bubble charts

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -18,13 +18,76 @@ function buildMetadata(sample) {
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
+  url = `/samples/${sample}`;
+
+  d3.json(url).then(function(data) {
+    console.log(data);
+    // var sorted_data = [data];
+
+    //sort our information first
+    // sorted_data.sort(function(a,b){
+    //   return parseFloat(b.sample_values) - parseFloat(a.sample_values)
+    // });
+
+    // console.log(sorted_data)
+
+    // Grab values from the response json object to build the plots
+    //pull only first 10 values
+    var otu_ids = data.otu_ids.slice(0,10);
+    //test variable pull
+    console.log(otu_ids);
+    var otu_labels = data.otu_labels.slice(0,10);
+    console.log(otu_labels);
+    var sample_values = data.sample_values.slice(0,10);
+    console.log(sample_values);
+
+    //create variable for length of otu_labels
+    otu_labels_lengths = otu_labels
+
+
+    //Build a Pie Chart
+    var trace1 = {
+      values: sample_values,
+      labels: otu_ids,
+      hoverinfo: otu_labels,
+      type: "pie"
+    };
+
+    var pieData = [trace1];
+
+    //layout
+    var pieLayout = {
+      // title: `Sample: ${sample}, Top 10 `
+      title: `${sample}`
+    }
+
+    Plotly.plot("pie",pieData, pieLayout);
 
     // @TODO: Build a Bubble Chart using the sample data
+    var trace2 = {
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: 'markers',
+      marker: {
+        size: sample_values,
+        color: otu_ids
+      }
+    };
 
-    // @TODO: Build a Pie Chart
-    // HINT: You will need to use slice() to grab the top 10 sample_values,
-    // otu_ids, and labels (10 each).
-}
+    var bubbleData = [trace2]
+
+    var bubbleLayout = {
+      title: ` Sample: ${sample} Biodiversity Bubble Chart`,
+      xaxis: {
+        title: "OTU ID"
+      }
+    }
+
+    Plotly.plot('bubble',bubbleData, bubbleLayout);
+
+  })
+};
 
 function init() {
   // Grab a reference to the dropdown select element
@@ -54,3 +117,5 @@ function optionChanged(newSample) {
 
 // Initialize the dashboard
 init();
+
+
